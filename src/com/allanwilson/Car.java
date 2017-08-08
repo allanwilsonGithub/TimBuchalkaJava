08403c298dd779ef314c98da2a4a57ff48fdbc6a
Allan Wilson <allan.wilson@vaisala.com>
2017-08-09 00:18:01 +0300

    testing IntellijIDEA commiting

---
+++ src/com/allanwilson/Car.java
@@ -0,0 +1,13 @@
+package com.allanwilson;
+
+/**
+ * Created by awil on 2017-08-09. Starting course again after summer
+ */
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+}
--- src/com/allanwilson/Main.java
+++ src/com/allanwilson/Main.java
@@ -3,6 +3,7 @@ package com.allanwilson;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Car porsche = new Car();
+        Car holden = new Car();
     }
 }
